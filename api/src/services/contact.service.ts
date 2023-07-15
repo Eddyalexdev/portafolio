0ad285990c277beb94e05c1db5702a2a2c51f9b8
Eddyalexdev <Eddyalex.dev@gmail.com>
2023-07-14 21:20:38 -0400

    mejoras en el frontend y backend

--- api/src/services/contact.service.ts
+++ api/src/services/contact.service.ts
@@ -1,7 +1,29 @@
 import { IContact } from "../intefaces/contact.interface";
 import contactModel from "../model/contact.model";
+import nodemailer from 'nodemailer'
+import "dotenv/config"
 
 export const sendMessage = async (data: IContact) => {
   const contact = await contactModel.create(data) 
-  return contact
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.forwardemail.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.USER_MAIL || "",
+      pass: process.env.PASS_MAIL || ""
+    }
+  })
+
+  const message = await transporter.sendMail({
+    from: contact.email,
+    to: 'Eddyalex.dev@gmail.com',
+    subject: 'Contact with portfolio',
+    html: `<h1>${contact.name}</h1>
+          <p>${contact.message}</p>
+    `
+  })
+
+  return message
 }
